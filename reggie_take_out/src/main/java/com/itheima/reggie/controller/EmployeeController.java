e6849c57056ab09a837dc62f9728a85ff4324453
hcwang18 <hcwang18@iflytek.com>
2022-05-21 17:32:39 +0800

    feat: 退出登陆

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -60,4 +60,13 @@ public class EmployeeController {
         return R.success(emp);
     }
 
+    /**
+     * 员工推出
+     */
+    @PostMapping("/logout")
+        public R<String> logout(HttpServletRequest request){
+        request.getSession().removeAttribute("employee");
+          return R.success("退出成功");
+        }
+
 }
